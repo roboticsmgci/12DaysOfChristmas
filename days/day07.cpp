eadd3a6832d82ce14a8bd4f599c4ac5ce3b3aff2
Anish Nagariya <anishnagariya07@gmail.com>
2022-01-17 10:11:53 -0500

    Fixed header file, added space to line 9 in day07.cpp, semi colon to line 7 day07.cpp

---
+++ days/day07.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "day07.h"
+
+using namespace std;
+
+string Day07(int day){
+    string line = "";
+    if (day >= 7){
+        line = "Seven swans a swimming \n";
+    }
+    return line;
+
+}
